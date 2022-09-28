e7168341f0c4ec0bd3b62ea42bf9169e4d359d2d
Maurice <bmaurice@psu.edu>
2022-09-27 23:58:20 -0400

    Initial commit

---
+++ mappingQuant.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 22:16:27 2022
+
+@author: mpb5554
+"""
+
+import os
+import subprocess
+import sys
+import csv
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--bamDir",
+                    help="Folder containing bam files.", required=True)
+parser.add_argument("-o", "--outFile",
+                    help=f"Output file", required=True)
+args = parser.parse_args()
+
+
+def quantifyMap(bam):
+     
+    cmd1 = f'samtools view -c {bam}'
+    execute1 = subprocess.run(cmd1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    if execute1.returncode == 0:
+        all_reads = int(execute1.stdout.decode('utf-8').strip())
+    else:
+        print(f'Something is wrong with the bam file: {bam}')
+        sys.exit()
+
+    cmd2 = f'samtools view -c -F 260 {bam}'
+    execute2 = subprocess.run(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if execute2.returncode == 0:
+        map_reads = int(execute2.stdout.decode('utf-8').strip())#.split('\n')
+    else:
+        print(f'Something is wrong with the bam file: {bam}')
+        sys.exit()    
+    
+    percentMap = round((map_reads/all_reads)*100, 2)
+
+
+    return [all_reads, map_reads, percentMap]
+
+dataDir = args.bamDir
+mappReport = args.outFile
+
+samples = []
+for file in os.listdir(dataDir):
+    if file.endswith('_raw.bam'):
+        sample = file.strip('_raw.bam')
+        samples.append(sample)
+
+with open(mappReport, 'w') as report:
+    print('\n Generating mapping summary report\n')
+    writer = csv.writer(report, dialect = csv.excel_tab)
+    writer.writerow(['sample', 'rtot', 'rmapped', 'perc_rmapped', 'dtot', 'dmapped', 'perc_dmapped', 'perc_dtot', 'perc_dmapped'])
+    for sample in sorted(samples):
+        data = [sample]
+        raw = os.path.join(dataDir, f'{sample}_raw.bam')
+        dedup = os.path.join(dataDir, f'{sample}_dedupped.bam')
+        L = quantifyMap(raw)
+        M = quantifyMap(dedup)
+        N = [round(M[0]/L[0]*100, 2), round(M[1]/L[1]*100, 2)]
+        P = L + M + N
+        data.extend(P)
+        writer.writerow(data)
+        print(f'--- Added data for sample: {sample} ---')
+
+print('\nDone!\n')
+
+
+
+
+
+
+
+
+
+
---
+++ quickBlaster.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+import os
+import sys
+import argparse
+from Bio import SearchIO
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-q', '--query', help='Input fasta', required='True')
+parser.add_argument('-ht', '--hit_out', help='Output file for hits', required=True)
+parser.add_argument('-nh', '--no_hit', help='Output file for no hits', default='no_hit.tsv')
+parser.add_argument('-db', '--blastdb', help='Path to the blast database')
+parser.add_argument('-s', '--short', help='short query sequences', action='store_true')
+
+
+args = parser.parse_args()
+
+infile = args.query
+blast_results = args.hit_out
+base_dir = os.path.dirname(blast_results)
+if not os.path.exists(base_dir):
+    os.makedirs(base_dir)
+no_hits = os.path.join(base_dir, args.no_hit)
+blast_out = os.path.join(base_dir, 'blast_results.xml')
+cpus = int(int(os.cpu_count()) * 3 / 4)
+cmd = f'blastn -num_threads {cpus} -db {args.blastdb} -query {infile} -out {blast_out} -outfmt 5'
+cmd_s = f'blastn -num_threads {cpus} -task blastn-short -db {args.blastdb} -query {infile} -out {blast_out} -outfmt 5'
+if not os.path.exists(blast_out):
+    print("\n Runing BLAST. May take some time ......\n")
+    if args.short:
+        os.system(cmd_s)
+    else:
+        os.system(cmd)   
+else:
+    print("\n Blast results file exits. Extracting data from existing file.\n")
+
+if os.path.exists(blast_out):
+    with open(blast_results, "w") as results, open(no_hits, "w") as alt:
+        res_writer = csv.writer(results, dialect=csv.excel_tab)
+        alt_writer = csv.writer(alt, dialect=csv.excel_tab)
+        res_writer.writerow(['Query_id', 'Best hit', 'Accession #', 'Percent ID', 'Hit length', 'Hit start', 'Hit end', 'Query length', 'Query span',  'Query cov', 'Query start', 'Query end'])
+        alt_writer.writerow(['Query_id', 'Best hit', 'Accession #', 'Percent ID', 'Hit length', 'Query length', 'HSP length', 'Hit start', 'Hit end', 'Query cov', 'q_start', 'q_end'])
+        for record in SearchIO.parse(blast_out, "blast-xml"):
+
+            if not len(record.hits) == 0:
+                best_hit = record[0]
+                best_hsp = best_hit[0]
+                query_id = best_hsp.query_id
+                accession = best_hit.accession
+                os.system(f'blastdbcmd -db {args.blastdb} -entry {accession} -outfmt "%t" > log.txt')
+                with open('log.txt') as log:
+                    name = log.readline().strip()
+                hsp_len = best_hsp.aln_span
+                qstart = best_hsp.query_start + 1
+                qend = best_hsp.query_end
+                percent_id = str(round(((best_hsp.ident_num / best_hsp.aln_span) * 100), 2)) + "%"
+                HSPperQuery = str(best_hsp.aln_span) + "/" + str(record.seq_len)
+                hstart = best_hsp.hit_start + 1
+                hend = best_hsp.hit_end
+                qlen = record.seq_len
+                qcov = str(round(((best_hsp.query_span / qlen) * 100), 2)) + "%"
+                print(f' {query_id}::{accession}::{name}::{percent_id}::{qcov}')
+                print('\tHit start: ', best_hsp.hit_start_all)
+                print('\tHit end: ', best_hsp.hit_end_all)
+                print('\t--------------------------------')
+                print('\tQuery start: ', best_hsp.query_start_all)
+                print('\tQuery end: ', best_hsp.query_end_all)
+                print('\n')
+
+                res_writer.writerow([query_id, name, accession, percent_id, best_hit.seq_len, hstart, hend, qlen, best_hsp.query_span, qcov, qstart, qend])
+            else:
+                alt_writer.writerow([record.id, "None found", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"])
+    print(" Done!!\n")
+else:
+    print(" Blast results file was not found!\n")
+
+# print(f'{acession}:: {name}:: {percent_id}')
+# print('\tHit start: ', best_hsp.hit_start)
+# print('\tHit end: ', best_hsp.hit_end)
+# print('\t--------------------------------')
+# print('\tQuery start: ', best_hsp.hit_start)
+# print('\tQuery end: ', best_hsp.hit_all)
+# print('')
